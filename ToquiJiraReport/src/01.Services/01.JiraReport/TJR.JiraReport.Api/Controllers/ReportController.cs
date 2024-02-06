93ecfd19ee208aa8cdf9d3e26f17a533ad77e681
Lucas Choque <lucas@openix.com.ar>
2024-02-06 11:36:23 -0300

    initial project

---
+++ ToquiJiraReport/src/01.Services/01.JiraReport/TJR.JiraReport.Api/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TJR.JiraReport.Api.Controllers
+{
+    
+    [Route("api/report")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet("hc")]
+        public string HealthCheck()
+        {
+            return "Working...";
+        }
+    }
+}
